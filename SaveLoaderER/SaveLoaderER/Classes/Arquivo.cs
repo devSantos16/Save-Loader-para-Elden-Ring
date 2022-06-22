9fad64dd73d0251cb7ca7127eaf3659aa17fad1b
Emanuel dos Santos Paz <devsantos1999@gmail.com>
2022-06-21 21:25:59 -0300

    Orientação a objeto

--- SaveLoaderER/SaveLoaderER/Classes/Arquivo.cs
+++ SaveLoaderER/SaveLoaderER/Classes/Arquivo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,46 @@ namespace SaveLoaderER.Classes
 {
     internal class Arquivo
     {
-        public int nomearquivo { get; set; }
+        public string save_path { get; set; }
+        public string all_save_path { get; set; }
 
         public Arquivo()
         {
-            
+            all_save_path = @"C:\Users\nel_2\OneDrive\Resto\Documentos\SAVEFILE_ELDENRING";
+        }
+
+        public string AcessarArquivosSalvado(string all_save_path)
+        {
+            string[] dirs = Directory.GetDirectories(all_save_path, "*", SearchOption.TopDirectoryOnly);
+            string nomePasta = string.Empty;
+
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                nomePasta = dirs[i];
+                string concat = all_save_path + "\\";
+                nomePasta = nomePasta.Replace(concat, "");
+                return nomePasta;
+            }
+
+            return "Erro";
+        }
+
+        public void AdicionarPastaEArquivos(string save_path, string all_save_path)
+        {
+
+            string[] files = Directory.GetFiles(save_path);
+            string nomeArquivo, caminhoArquivo;
+
+            int numeroAspasDiretorio = save_path.Split(@"\").Length;
+            string novoDiretorioArquivoEldenRing = all_save_path + @"\" + save_path.Split(@"\")[numeroAspasDiretorio - 1];
+
+            Directory.CreateDirectory(novoDiretorioArquivoEldenRing);
+            foreach (string file in files)
+            {
+                nomeArquivo = Path.GetFileName(file);
+                caminhoArquivo = Path.Combine(novoDiretorioArquivoEldenRing, nomeArquivo);
+                File.Copy(file, caminhoArquivo, true);
+            }
         }
     }
 }
--- SaveLoaderER/SaveLoaderER/MainWindow.xaml.cs
+++ SaveLoaderER/SaveLoaderER/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SaveLoaderER.Classes;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -21,52 +22,21 @@ namespace SaveLoaderER
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string DIRETORIO_ARQUIVO_ELDEN_RING = @"C:\Users\nel_2\OneDrive\Resto\Documentos\SAVEFILE_ELDENRING";
         private const string DIRETORIO_ARQUIVO_SAVE = @"C:\Users\nel_2\OneDrive\Resto\Área de Trabalho\76561198900787417";
-
-
-        //private const string DIRETORIO_ARQUIVO_CACHE = @"C:\Users\nel_2\AppData\Roaming\SAVEFILE_ELDENRING_CACHE";
 
         public MainWindow()
         {
             InitializeComponent();
 
-            AdicionarPastaEArquivos(DIRETORIO_ARQUIVO_SAVE, DIRETORIO_ARQUIVO_ELDEN_RING);
-            AcessarArquivos(DIRETORIO_ARQUIVO_ELDEN_RING);
-        }
+            Arquivo SaveFile = new Arquivo();
+            SaveFile.save_path = DIRETORIO_ARQUIVO_SAVE;
 
-        private static string AcessarArquivos(string diretorioArquivo)
-        {
-            string[] dirs = Directory.GetDirectories(diretorioArquivo, "*", SearchOption.TopDirectoryOnly);
-            string nomePasta = string.Empty;
+            SaveFile.AdicionarPastaEArquivos(SaveFile.save_path, SaveFile.all_save_path);
+            SaveFile.AcessarArquivosSalvado(SaveFile.save_path);
+            
 
-            for (int i = 0; i < dirs.Length; i++)
-            {
-                nomePasta = dirs[i];
-                string concat = diretorioArquivo + "\\";
-                nomePasta = nomePasta.Replace(concat, "");
-                return nomePasta;
-            }
 
-            return "Erro";
-        }
 
-        private static void AdicionarPastaEArquivos(string diretorioArquivoSave, string diretorioArquivoEldenRing)
-        {  
-
-            string[] files = Directory.GetFiles(diretorioArquivoSave);
-            string nomeArquivo, caminhoArquivo;
-
-            int numeroAspasDiretorio = diretorioArquivoSave.Split(@"\").Length;
-            string novoDiretorioArquivoEldenRing = diretorioArquivoEldenRing + @"\" + diretorioArquivoSave.Split(@"\")[numeroAspasDiretorio - 1];
-
-            Directory.CreateDirectory(novoDiretorioArquivoEldenRing);
-            foreach (string file in files)
-            {
-                nomeArquivo = System.IO.Path.GetFileName(file);
-                caminhoArquivo = System.IO.Path.Combine(novoDiretorioArquivoEldenRing, nomeArquivo);
-                File.Copy(file, caminhoArquivo, true);
-            }   
         }
     }
 }
